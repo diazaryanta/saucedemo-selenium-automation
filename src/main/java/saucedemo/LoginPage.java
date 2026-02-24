35ae666a4325d0f8538c24a0143dbf2ccdcd838d
Mirnawaty <diazaryanta196@gmail.com>
2026-02-24 13:34:41 +0700

    Initial commit

---
+++ src/main/java/saucedemo/LoginPage.java
@@ -0,0 +1,115 @@
+package saucedemo;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import core.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration; // Untuk mengatur waktu tunggu (Duration.ofSeconds)
+
+
+public class LoginPage extends BasePage {
+
+
+    private static final Logger log = LogManager.getLogger(LoginPage.class);
+
+
+    @FindBy(id = "user-name")
+    private WebElement usernameInput;
+
+
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+
+
+    @FindBy(id = "login-button")
+    private WebElement loginButton;
+
+
+    @FindBy(css = "[data-test='error']")
+    private WebElement errorAlert;
+
+
+    @FindBy(className = "title")
+    private WebElement pageTitle;
+
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+        log.info("LoginPage initialized");
+    }
+
+
+    public void login(String username, String password) {
+        log.info("Attempting to login with username: {}", username);
+        waitForElementToBeVisible(usernameInput);
+        usernameInput.sendKeys(username);
+        log.info("Username entered");
+        passwordInput.sendKeys(password);
+        log.info("Password entered");
+        loginButton.click();
+        log.info("Login button clicked");
+    }
+
+
+    public boolean isUserLoggedInSuccessfully() {
+        try {
+            // Tambahkan WebDriverWait agar Selenium menunggu halaman dimuat
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            // Tunggu sampai element judul "Products" terlihat di layar
+            WebElement title = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("title")));
+
+            return title.isDisplayed() && title.getText().equalsIgnoreCase("Products");
+        } catch (Exception e) {
+            // Jika dalam 10 detik tidak muncul, berarti memang gagal login
+            return false;
+        }
+    }
+
+
+    public boolean isErrorMessageDisplayed() {
+        log.info("Checking if error message is displayed");
+        try {
+            waitForElementToBeVisible(errorAlert);
+            boolean isDisplayed = errorAlert.isDisplayed();
+            if (isDisplayed) {
+                log.info("Error message is displayed");
+            } else {
+                log.info("Error message is not displayed");
+            }
+            return isDisplayed;
+        } catch (Exception e) {
+            log.info("No error message found");
+            return false;
+        }
+    }
+
+
+    public String getErrorMessage() {
+        log.info("Getting error message text");
+        try {
+            waitForElementToBeVisible(errorAlert);
+            String message = errorAlert.getText();
+            log.info("Error message: {}", message);
+            return message;
+        } catch (Exception e) {
+            log.error("Failed to get error message: {}", e.getMessage());
+            return "";
+        }
+    }
+
+
+    public String getCurrentUrl() {
+        String url = driver.getCurrentUrl();
+        log.info("Current URL: {}", url);
+        return url;
+    }
+
+
+}
